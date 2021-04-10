58c14456ab74cc52bfaf5f890a6e45994f4d5c2b
vprada25 <v.prada@udla.edu.co>
2021-04-09 20:02:39 -0500

    feat(view): view capital

---
+++ src/app/countries/pages/capital/capital.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-capital',
+  templateUrl: './capital.component.html',
+  styles: [
+  ]
+})
+export class CapitalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
