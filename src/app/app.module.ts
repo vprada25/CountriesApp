23208dcb2b3a2fca0c72b3ff43e0c2c6c497fb74
vprada25 <v.prada@udla.edu.co>
2021-04-09 20:06:17 -0500

    feat(module) : configuration module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+
 
 import { AppComponent } from './app.component';
+import { CountriesModule } from './countries/countries.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    CountriesModule,
+    SharedModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
