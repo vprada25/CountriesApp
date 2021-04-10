a6310ef5cc5619a09878ea5522d2b4aa15d16ad7
vprada25 <v.prada@udla.edu.co>
2021-04-09 20:02:55 -0500

    feat(view): view region

---
+++ src/app/countries/pages/region/region.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-region',
+  templateUrl: './region.component.html',
+  styles: [
+  ]
+})
+export class RegionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
