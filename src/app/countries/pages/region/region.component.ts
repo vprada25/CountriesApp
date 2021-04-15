18064ad112199db890f39e2c5930174ae8c7ee86
vprada25 <v.prada@udla.edu.co>
2021-04-14 22:25:43 -0500

    feat(searRegion): add search region in component region.

--- src/app/countries/pages/region/region.component.ts
+++ src/app/countries/pages/region/region.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CountryService } from '../../service/country.service';
+import { Country } from '../../interfaces/countrie.interface';
 
 @Component({
   selector: 'app-region',
@@ -14,12 +17,35 @@ export class RegionComponent  {
 
   region: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
   regionActive: string= ''
+  country : Country[] = [];
 
-  constructor() { }
+  constructor(private countryService: CountryService) { }
 
   activeRegion (arg : string){
     this.regionActive = arg;
   }
+
+  getClassCss (arg: string):string{
+    return (arg=== this.regionActive) ? 'btn btn-primary': 'btn btn-outline-primary'
+  }
+
+
+  search(termino: string) {
+    
+    this.countryService.searchRegion(termino)
+      .subscribe((res) => {
+        console.log(res)
+        this.country = res;
+
+      },
+        (error) => {
+          
+          this.country = []
+        }
+
+      )
+  }
+
 
   
 }
